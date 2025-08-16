2f6cc197130228f6985741da3cffcf089a5ab790
Abhinandan Subedi <devloperabhinandan@gmail.com>
2025-08-16 13:07:08 +0545

    Final Production

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
+import "./app.css";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import About from "./Components/About";
