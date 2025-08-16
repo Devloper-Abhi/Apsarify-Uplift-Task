4035445a5b7cfac047ee635e2764e20aaf11ec8f
Abhinandan Subedi <devloperabhinandan@gmail.com>
2025-08-16 13:37:57 +0545

    Add app.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./src/app.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+  import React from "react";
+  import { StrictMode } from "react";
+  import { createRoot } from "react-dom/client";
+  import "./app.css";
+  import App from "./App.jsx";
+  import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-);
+  createRoot(document.getElementById("root")).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  );
