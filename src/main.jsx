c553d1ff01a1d228b69ce821f857b5d2f55a3c7d
Abhinandan Subedi <devloperabhinandan@gmail.com>
2025-08-16 15:41:19 +0545

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/Apsarify-Uplift-Task">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
