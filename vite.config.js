ec4fd1a16dff9e7af4f069272c19530366b9d420
Abhinandan Subedi <devloperabhinandan@gmail.com>
2025-08-16 14:32:00 +0545

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    tailwindcss()
   ],
+  base: "/Apsarify-Uplift-Task"
 })
